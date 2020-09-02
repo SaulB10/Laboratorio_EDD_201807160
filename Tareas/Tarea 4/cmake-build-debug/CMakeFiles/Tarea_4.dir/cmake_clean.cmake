a0b7284f8d9e6726b107b41d31bbcaebbe1fba3a
SaulB10 <69322283+SaulB10@users.noreply.github.com>
2020-09-01 18:53:16 -0600

    Add files via upload

---
+++ Tareas/Tarea 4/cmake-build-debug/CMakeFiles/Tarea_4.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tarea_4.dir/main.cpp.obj"
+  "Tarea_4.exe"
+  "Tarea_4.exe.manifest"
+  "Tarea_4.pdb"
+  "libTarea_4.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tarea_4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Tareas/Tarea 4/main.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+using namespace std;
+string Palabras[5]={"Saul","Barillas","hola","como estas","bien"};
+int ascii[5];
+void ArrayString(string Palabras[],int lenght);
+
+int main()
+{
+    int lenght=sizeof(Palabras)/sizeof(Palabras[0]);
+
+    ArrayString(Palabras,lenght);
+    return 0;
+}
+
+void ArrayString(string Palabras[],int lenght ){
+    int n;
+    string caracter;
+    int valor=0;
+    int valores[lenght];
+    int nuevo[lenght];
+    int aux,pos;
+    string ordenado[lenght];
+
+    for (int i = 0; i < lenght; ++i) {
+        cout<<Palabras[i]<<endl;
+        for (int j = 0; j <Palabras[i].length() ; ++j) {
+
+            caracter=Palabras[i];
+            //cout<<caracter[j]<<endl;
+            cout<<"Valor de "<<caracter[j]<<" es "<<int(caracter[j])<<endl;
+            valor+=int(caracter[j]);
+        }
+        cout<<valor<<endl;
+        valores[i]=valor;
+        nuevo[i]=valor;
+        valor=0;
+
+    }
+    cout<<"Array Original: "<<endl;
+    for (int i = 0; i <lenght ; ++i) {
+
+        cout<<valores[i]<<endl;
+
+    }
+    cout<<"-------------------------------"<<endl;
+
+
+//metodo de inserción
+    for (int i = 0; i <lenght ; ++i) {
+        aux=nuevo[i];
+        pos=i;
+        while ((pos>0) && nuevo[pos-1]>aux){
+            nuevo[pos]=nuevo[pos-1];
+            pos--;
+        }
+        nuevo[pos]=aux;
+    }
+    cout<<"Array ordenado: "<<endl;
+    for (int i = 0; i <lenght ; ++i) {
+
+        cout<<nuevo[i]<<endl;
+
+    }
+    cout<<"-------------------------------"<<endl;
+    //  cambio
+    for (int i = 0; i <lenght ; ++i) {
+        for (int j = 0; j < lenght; ++j) {
+            if(nuevo[i]==valores[j]){
+                cout<<"posicion en viejo: "<<j<<endl;
+                ordenado[i]=Palabras[j];
+
+            }
+        }
+
+    }
+    cout<<"Array ordenado de Palabras: "<<endl;
+    for (int i = 0; i <lenght ; ++i) {
+
+        cout<<ordenado[i]<<endl;
+
+    }
+    cout<<"-------------------------------"<<endl;
+
+
+
+
+
+
+
+
+
+}
+/*
+void insercion(int*a,int lenght){
+int aux,pos;
+    for (int i = 0; i <lenght ; ++i) {
+        aux=a[i];
+        pos=i;
+        while ((pos>0) && a[pos-1]>aux){
+            a[pos]=a[pos-1];
+            pos--;
+        }
+        a[pos]=aux;
+
+
+    }
+}*/
